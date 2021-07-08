df6907b201732e9935bbe642652d3e2a5343da5b
wwmingly <2991374415@qq.com>
2021-07-08 17:12:14 +0800

    docs readme.md

---
+++ README.md
@@ -0,0 +1,26 @@
+* 拉取项目
+git clone https://github.com/wwmingly/vvmily-node.git
+
+* 安装依赖
+```
+cd ./vvmily-node
+
+npm install
+```
+* 启动项目
+```
+npm run dev 
+
+npm run devmon         // 热更新
+```
+* 数据库，对应两表 bloglist和 users以及对应的字段 key，练习数据库表结构和数据（一般不这么干！！！）已导出存放在`mysql-table`文件中，需要可自行导入
+![](https://git.kancloud.cn/repos/vvmily_king/vvmily/raw/4fb75802d481fba6b7697f72e6dd66758395e41e/images/screenshot_1625730082528.png?access-token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjU3NTcyNDAsImlhdCI6MTYyNTcxNDA0MCwicmVwb3NpdG9yeSI6InZ2bWlseV9raW5nXC92dm1pbHkiLCJ1c2VyIjp7InVzZXJuYW1lIjoidnZtaWx5X2tpbmciLCJuYW1lIjoidnZtaWx5S2luZyIsImVtYWlsIjoiMjk5MTM3NDQxNUBxcS5jb20iLCJ0b2tlbiI6IjUzM2FmNzU2M2JkY2JhYTZjMmYyNGNiNzI0YTBmNDNjIiwiYXV0aG9yaXplIjp7InB1bGwiOnRydWUsInB1c2giOnRydWUsImFkbWluIjp0cnVlfX19.RzROQfUHi6U0odyasy0ks2UwcHVN0Xn_-KOYVS7LFzI)
+*****
+* 启动数据库，更多命令请点击 [这里](https://www.kancloud.cn/vvmily_king/vvmily/2331779) 查看
+```
+net start mysql  // 启动
+
+mysql -u root -p  // 登录
+```
+等你启动，看到下图，项目搭建算是大功告成
+![](blob:https://www.kancloud.cn/0fe25c5b-c2e4-4f66-b472-5be5d8ee74ad)
---
+++ mysql-table/mynode_users.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mynode
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userid` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `realname` varchar(10) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `createtime` bigint NOT NULL DEFAULT '0',
+  `state` int NOT NULL DEFAULT '1',
+  `age` int DEFAULT NULL,
+  `sex` varchar(2) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'zhangsan','张三','123123',1625558175332,1,22,'男'),(2,'lisi','李四','123123',1625624026412,1,22,'男'),(3,'wangwu','王五','22334455',1625624088234,0,18,'女'),(5,'zhangsan','张三2','224455',1625624088234,1,23,'女');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-08 16:12:34
