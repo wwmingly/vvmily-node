bcc5381565c9b876d76be377040aa814b453affb
wwmingly <2991374415@qq.com>
2021-07-08 15:32:38 +0800

    update: listen

--- bin/www.js
+++ bin/www.js
@@ -1,9 +1,13 @@
 const http = require("http")
 
 const PORT = 8900
+const HOST = '127.0.0.1'
 
 const serverHandle = require("../app.js")
 
 const server = http.createServer(serverHandle)
 
-server.listen(PORT)
+server.listen(PORT, HOST, () => {
+    console.log(`I'm listening at  http://localhost:${PORT}`);
+    console.log(`I'm listening at  http://${HOST}:${PORT}`);
+});
